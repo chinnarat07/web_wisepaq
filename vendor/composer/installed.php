60ea53bd72a8e13413ff2b58e6490a29d4941e3c
chinnarat07 <bu.chinnarat@gmail.com>
2024-12-20 23:30:20 +0700

    Include vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chinnaratbutjang/web_wisepaq',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f4248410bbe7982aa413ab8dd0ac598c8c04aecb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chinnaratbutjang/web_wisepaq' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f4248410bbe7982aa413ab8dd0ac598c8c04aecb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
